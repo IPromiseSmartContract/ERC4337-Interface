407fab0756f54d12949c853dac5133bb811ba6c4
alan890104 <alan890104@gmail.com>
2023-06-10 01:23:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Home from "./pages/Home.vue";
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped></style>
